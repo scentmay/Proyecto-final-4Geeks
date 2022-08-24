eda3a7a4e3aefa27fa7b15f5fdcfdd943b44eed9
scentmay <scentmay@gmail.com>
2022-08-24 19:11:29 +0000

    Modelado BBDD inicial listo

--- src/api/models.py
+++ src/api/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
@@ -17,3 +18,41 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+class Cliente(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), unique=False, nullable=False)
+    userName = db.Column(db.String(120), unique=False, nullable=False)
+    lastName = db.Column(db.String(120), unique=False, nullable=False)
+    dni = db.Column(db.String(120), unique=True, nullable=False)
+    direccion = db.Column(db.String(120), unique=False, nullable=False)
+    telefono = db.Column(db.String(120), unique=False, nullable=False)
+    objetivo = db.Column(db.String(120), unique=False, nullable=False)
+    peso = db.Column(db.Float(20), unique=False, nullable=False)
+    corrienteDePago = db.Column(db.Boolean, unique=False, nullable=False)
+    fechaDeAlta = db.Column(db.DateTime, unique=False, nullable=False)
+
+class Entreno(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    cliente_id = db.Column(db.Integer, db.ForeignKey('cliente.id'))
+    fechaEntreno = db.Column(db.DateTime, default = datetime.now())
+
+class EntrenoContiene(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    id_ejercicio = db.Column(db.Integer, primary_key=True)
+    series = db.Column(db.Integer, unique=False, nullable=False)
+    repeticiones = db.Column(db.Integer, unique=False, nullable=False)
+    peso = db.Column(db.Integer, unique=False, nullable=False)
+
+class Ejercicio(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    categoria_id = db.Column(db.Integer, db.ForeignKey('categoria.id'))
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
+    descripcion = db.Column(db.String(120), unique=False, nullable=False)
+    video = db.Column(db.String(120), unique=False, nullable=False)
+    dificultad = db.Column(db.String(120), unique=False, nullable=False)
+
+class Categoria(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(120), unique=False, nullable=False)
